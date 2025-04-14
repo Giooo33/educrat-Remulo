054e55da81f9d742b94ea8ee1d023227a23eb1b1
Giovana <giovanadutramaga@gmail.com>
2025-04-14 12:12:27 -0300

    1404

---
+++ README.md
@@ -0,0 +1,53 @@
+Relatório da aula do dia 14 de abril de 2025.
+
+	O professor Remulo passou em sala um código denominado “educrat” um site de cursos onlines. Sendo que, o código veio pronto e os alunos deveriam corrigi-lo.
+	Diante disso, a primeira etapa foi analisar o projeto com o intuito de compreendê-lo, além de executar o comando “npm install” a fim de iniciar o programa e vê-lo com o comando “npm run dev”, com a visualização do site fica mais fácil a correção e ajustes do código. 
+	Na segunda etapa era a de manter as seguintes páginas: 
+Home 1
+
+
+About
+
+
+Contact US
+
+
+Courses List V4
+
+
+Course V6
+
+
+Course Cart
+
+
+Course Checkout
+
+
+Lesson Page 2
+
+
+Blog 3
+
+
+Blog Single
+
+
+Dashboard Completo
+
+
+Eventos 2
+
+
+Event Cart
+
+
+Event Checkout
+
+	Foram excluídas as seguintes páginas: 
+
+	Além disso, removemos as rotas das páginas não listadas, excluímos os componentes e arquivos das páginas não utilizadas e eliminamos importações associadas nos arquivos onde estavam sendo utilizadas.
+	Na terceira etapa, transformamos a página Home em um botão único sem dar outras opções a não ser o “Home1” agora “Home”. Os textos e estilos, optei em ajustá-los posteriormente. Na sequência, na página cursos, retiramos a coluna curso populares e também a opção explorar, nos arquivos do código. Depois apagamos a página pages do site no Menu do site, retirando o botão.  
+
+
+
---
+++ src/data/menu.js
@@ -0,0 +1,122 @@
+export const menuList = [
+  {
+    title: "Home",
+    links: [
+      { href: "/", label: "Home" },
+    ],
+  },
+  {
+    title: "Courses",
+    links: [
+      {
+        title: "Course List Layouts",
+        links: [
+          { href: "/courses-list-4", label: "Course List v4" },
+        ],
+      },
+      {
+        title: "Course Single Layouts",
+
+        links: [
+          { href: "/courses-single-6/7", label: "Course Single v6" },
+          { href: "/course-cart", label: "Course Cart" },
+          { href: "/course-checkout", label: "Course Checkout" },
+        ],
+      },
+      {
+        title: "About Courses",
+        links: [
+          { href: "/lesson-single-2", label: "Lesson Page v2" },
+        ],
+      },
+      {
+        title: "Dashboard Pages",
+        links: [
+          { href: "/dashboard", label: "Dashboard" },
+          { href: "/dshb-courses", label: "My Courses" },
+          { href: "/dshb-bookmarks", label: "Bookmarks" },
+          { href: "/dshb-listing", label: "Add Listing" },
+          { href: "/dshb-reviews", label: "Reviews" },
+          { href: "/dshb-settings", label: "Settings" },
+          { href: "/dshb-administration", label: "Administration" },
+          { href: "/dshb-assignment", label: "Assignment" },
+          { href: "/dshb-calendar", label: "Calendar" },
+          { href: "/dshb-dashboard", label: "Single Dashboard" },
+          { href: "/dshb-dictionary", label: "Dictionary" },
+          { href: "/dshb-forums", label: "Forums" },
+          { href: "/dshb-grades", label: "Grades" },
+          { href: "/dshb-messages", label: "Messages" },
+          { href: "/dshb-participants", label: "Participants" },
+          { href: "/dshb-quiz", label: "Quiz" },
+          { href: "/dshb-survey", label: "Survey" },
+        ],
+      },
+      {
+        title: "",
+        links: [
+          { label: "", href: "#" },
+          { label: "", href: "#" },
+          { label: "", href: "#" },
+          { label: "", href: "#" },
+          { label: "", href: "#" },
+          { label: "", href: "#" },
+          { label: "", href: "#" },
+        ],
+      },
+      // {
+      //     title : 'Course List Layouts',
+      //     links: [
+
+      //     ]
+
+      // },
+    ],
+  },
+  {
+    title: "Events",
+    links: [
+      { href: "/event-list-2", label: "Event List 2" },
+      { href: "/events/1", label: "Event Single" },
+      { href: "/event-cart", label: "Event Cart" },
+      { href: "/event-checkout", label: "Event Checkout" },
+    ],
+  },
+  {
+    title: "Blogs",
+    links: [
+      { href: "/blog-list-3", label: "Blog List 3" },
+      { href: "/blogs/3", label: "Blog Single" },
+    ],
+  },
+  {
+    title: "Pages",
+    links: [
+      {
+        title: "About Us",
+        links: [
+          { href: "/about-1", label: "About 1" },
+          { href: "/about-2", label: "About 2" },
+        ],
+      },
+      {
+        title: "Contact",
+        links: [
+          { href: "/contact-1", label: "Contact 1" },
+          { href: "/contact-2", label: "Contact 2" },
+        ],
+      },
+      {
+        title: "",
+        links: [
+        ],
+      },
+      { href: "/pricing", label: "Membership plans" },
+      { href: "/not-found", label: "404 Page" },
+      { href: "/terms", label: "FAQs" },
+      { href: "/help-center", label: "Help Center" },
+      { href: "/login", label: "Login" },
+      { href: "/signup", label: "Register" },
+      { href: "/ui-elements", label: "UI Elements" },
+    ],
+  },
+];
